8706c8378d2ee3c029dde8f978786f6d97d43a88
Tiago Amaro <tiagopadrela@gmail.com>
2019-12-27 15:44:36 -0300

    FPS implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 mod utils;
 
-use rand::prelude::*;
 use std::fmt;
 use wasm_bindgen::prelude::*;
+use web_sys::console;
 
 extern crate web_sys;
 
@@ -10,6 +10,23 @@ extern crate web_sys;
 macro_rules! log {
     ( $( $t:tt )* ) => {
         web_sys::console::log_1(&format!( $( $t )* ).into());
+    }
+}
+
+pub struct Timer<'a> {
+    name: &'a str,
+}
+
+impl<'a> Timer<'a> {
+    pub fn new(name: &'a str) -> Timer<'a> {
+        console::time_with_label(name);
+        Timer { name }
+    }
+}
+
+impl<'a> Drop for Timer<'a> {
+    fn drop(&mut self) {
+        console::time_end_with_label(self.name);
     }
 }
 
@@ -63,6 +80,7 @@ impl Universe {
 #[wasm_bindgen]
 impl Universe {
     pub fn tick(&mut self) {
+        // let _timer = Timer::new("Universe::tick");
         let mut next = self.cells.clone();
 
         for row in 0..self.height {
--- www/index.js
+++ www/index.js
@@ -22,6 +22,49 @@ const playPauseButton = document.getElementById("play-pause");
 const randomButton = document.getElementById("random");
 const tickSpeed = document.getElementById("tick-speed");
 
+const fps = new class {
+    constructor() {
+        this.fps = document.getElementById("fps");
+        this.frames = [];
+        this.lastFrameTimeStamp = performance.now();
+    }
+
+    render() {
+        // Convert the delta time since the last frame render into a measure
+        // of frames per second.
+        const now = performance.now();
+        const delta = now - this.lastFrameTimeStamp;
+        this.lastFrameTimeStamp = now;
+        const fps = 1 / delta * 1000;
+
+        // Save only the latest 100 timings.
+        this.frames.push(fps);
+        if (this.frames.length > 100) {
+            this.frames.shift();
+        }
+
+        // Find the max, min, and mean of our 100 latest timings.
+        let min = Infinity;
+        let max = -Infinity;
+        let sum = 0;
+        for (let i = 0; i < this.frames.length; i++) {
+            sum += this.frames[i];
+            min = Math.min(this.frames[i], min);
+            max = Math.max(this.frames[i], max);
+        }
+        let mean = sum / this.frames.length;
+
+        // Render the statistics.
+        this.fps.textContent = `
+Frames per Second:
+latest = ${Math.round(fps)}
+avg of last 100 = ${Math.round(mean)}
+min of last 100 = ${Math.round(min)}
+max of last 100 = ${Math.round(max)}
+        `.trim();
+    }
+};
+
 // Give the canvas room for all of our cells and a 1px border
 // around each of them.
 canvas.height = (CELL_SIZE + 1) * height + 1;
@@ -33,6 +76,8 @@ let animationId = null;
 
 
 const renderLoop = () => {
+    fps.render();
+
     for (let tick = 0; tick < tickSpeed.value; tick++) {
         universe.tick();
     }
